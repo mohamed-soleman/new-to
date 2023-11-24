ff477f542f739664bf5d0ece3e9eabf586694c32
mohamed soleman <mohamed_soleman2020@yahoo.com>
2023-11-24 19:12:31 +0200

    iam set php code

--- p.php
+++ p.php
@@ -1,4 +1,16 @@
+<?php
+    $number = 8;
+    if($number > 3 && $number < 6):
+        echo "this number  = 4";
+    elseif($number > 6 && $number < 9):
+        echo "this is number  = 8";
+    else:
+        echo "this is number = 10";
 
+    endif;
+    echo "<br>";
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
